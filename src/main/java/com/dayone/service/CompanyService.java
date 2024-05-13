857497a7d610df9c23e51a73335c0877469da09f
seungh22 <seunghee8898@naver.com>
2024-05-13 15:30:41 +0900

    first commit

---
+++ src/main/java/com/dayone/service/CompanyService.java
@@ -0,0 +1,102 @@
+package com.dayone.service;
+
+import com.dayone.exception.impl.NoCompanyException;
+import com.dayone.model.Company;
+import com.dayone.model.ScrapedResult;
+import com.dayone.persist.CompanyRepository;
+import com.dayone.persist.DividendRepository;
+import com.dayone.persist.entity.CompanyEntity;
+import com.dayone.persist.entity.DividendEntity;
+import com.dayone.scraper.Scraper;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.Trie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class CompanyService {
+
+    private final Trie trie;
+
+    private final Scraper yahooFinanceScraper;
+
+    private final CompanyRepository companyRepository;
+    private final DividendRepository dividendRepository;
+
+    public Company save(String ticker) {
+        boolean exists = this.companyRepository.existsByTicker(ticker);
+        if (exists) {
+            throw new RuntimeException("already exists ticker -> " + ticker);
+        }
+        return this.storeCompanyAndDividend(ticker);
+    }
+
+    public Page<CompanyEntity> getAllCompany(final Pageable pageable) {
+        return this.companyRepository.findAll(pageable);
+    }
+
+    private Company storeCompanyAndDividend(String ticker) {
+        // 1. ticker 를 기준으로 회사를 스크래핑
+        Company company = this.yahooFinanceScraper.scrapCompanyByTicker(ticker);
+        if (ObjectUtils.isEmpty(company)) {
+            throw new RuntimeException("failed to scrap ticker -> " + ticker);
+        }
+
+        // 2. 해당 회사가 존재할 경우, 회사의 배당금 정보를 스크래핑
+        ScrapedResult scrapedResult = this.yahooFinanceScraper.scrap(company);
+        // 3. 스크래핑 결과 반환
+        CompanyEntity companyEntity = this.companyRepository.save(new CompanyEntity(company));
+
+
+        List<DividendEntity> dividendEntities = scrapedResult.getDividends().stream()
+                .map(e -> new DividendEntity(companyEntity.getId(), e))
+                .collect(Collectors.toList());
+        this.dividendRepository.saveAll(dividendEntities);
+        return company;
+    }
+
+    public void addAutocompleteKeyword(String keyword) {
+        this.trie.put(keyword, null);
+    }
+
+    public List<String> autocomplete(String keyword) {
+        return (List<String>) this.trie.prefixMap(keyword).keySet()
+                .stream()
+                .collect(Collectors.toList());
+    }
+
+    public void deleteAutoCompleteKeyword(String keyword) {
+        this.trie.remove(keyword);
+    }
+
+    public List<String> getCompanyNamesByKeyword(String keyword) {
+        Pageable limit = PageRequest.of(0, 10);
+        Page<CompanyEntity> companyEntites = this.companyRepository.findByNameStartingWithIgnoreCase(keyword, limit);
+        return companyEntites.stream()
+                .map(e -> e.getName())
+                .collect(Collectors.toList());
+    }
+
+    public String deleteCompany(String ticker) {
+
+        var company = this.companyRepository.findByTicker(ticker)
+                                    .orElseThrow(() -> new NoCompanyException());
+        // 1. 배당금 정보 삭제
+        this.dividendRepository.deleteAllByCompanyId(company.getId());
+        // 2. 회사 정보 삭제
+        this.companyRepository.delete(company);
+
+        this.deleteAutoCompleteKeyword(company.getName());
+        return company.getName();
+    }
+
+}
